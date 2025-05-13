59097e2081dd27be1d61ae0439dd17d0539ea38b
Jairp03 <elvismartillo64@gmail.com>
2025-05-13 01:14:33 -0500

    proyecto de POO

--- core/views.py
+++ core/views.py
@@ -1,8 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
 
 # Create your views here.
 def home (request):
     return render(request, 'home.html')
+
+def signup (request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+        'form': UserCreationForm()
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            #registrar usuario
+            try:
+                user = User.objects.create_user(username = request.POST['username'], password= request.POST['password2'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                'form': UserCreationForm(),
+                "error": 'El usuario ya existe',
+                })
+        return render(request, 'signup.html', {
+        'form': UserCreationForm(),
+        "error": 'Las contraseñas no coinciden',
+        })
+def signin (request):
+    if request.method == 'GET':
+        return render (request, 'signin.html', {
+        'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
+        if user is None:
+            return render (request, 'signin.html', {
+            'form': AuthenticationForm,
+            'error': 'Usuario o contraseña incorrecta'
+            })
+        else:
+            login(request, user)
+            return redirect('home')
+
+def logoutuser(request):
+    logout(request)
+    return redirect('home')
 
 def create (request):
     pass
--- sistema_nomina/urls.py
+++ sistema_nomina/urls.py
@@ -23,4 +23,6 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('sign_in/', views.signin, name="signin"),
+    path('Sign_up/', views.signup, name="signup"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
